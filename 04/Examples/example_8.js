3a7df8365df1c05c58e5ddbe4b8cb84a82339623
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2021-01-01 12:51:11 -0600

    Add example_8.js

---
+++ 04/Examples/example_8.js
@@ -0,0 +1,7 @@
+// The Math object
+function randomPointOnCircle(radius) {
+    let angle = Math.random() * 2 * Math.PI;
+    return {x: radius * Math.cos(angle),
+            y: radius * Math.sin(angle)};
+}
+console.log(randomPointOnCircle(2));
